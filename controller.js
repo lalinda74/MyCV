c6492f51e37c7c41187e6169bd69e52ffcd2a6a4
Lalinda Sampath Dias <diaslalinda@gmail.com>
2016-02-29 23:04:38 +0530

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,5 +1,16 @@
-var application = angular.module('mainApp', []);
+var application = angular.module('mainApp', ['ngRoute']);
 
 application.controller('app', function($scope) {
 	
 });
+
+application.config(['$routeProvider', function($routeProvider) {
+        $routeProvider.
+            when('/tab1', {templateUrl: 'tab1.html', controller: HomeCtrl}).
+            when('/tab2', {templateUrl: 'tab2.html'}).
+            otherwise({redirectTo: '/'});
+}]);
+
+function HomeCtrl($scope) {
+    console.log("Done");
+}
